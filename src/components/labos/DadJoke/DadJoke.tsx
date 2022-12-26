94a22a73cb22e3ba03339edbd10c74c59a77fe3c
Van Dijck Alex (s131627) <alex.vandijck@student.ap.be>
2022-12-26 14:44:43 +0100

    Removed strictmode & Finshed dadjoke

--- src/components/labos/DadJoke/DadJoke.tsx
+++ src/components/labos/DadJoke/DadJoke.tsx
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useEffect, useState} from 'react';
 import { Button, Typography,useTheme,Grid } from '@mui/material';
 
 interface DadJoke {
@@ -8,6 +8,8 @@ interface DadJoke {
 const DadJoke = () => {
   const theme = useTheme();
   const [joke, setJoke] = useState<string>("");
+  const [favJoke,setFavJoke] = useState<string>('');
+ 
 
     const fetchJoke = async () => {
       const res = await fetch("https://icanhazdadjoke.com/", {
@@ -19,13 +21,26 @@ const DadJoke = () => {
       setJoke(data.joke);
     };
 
+    useEffect(()=>{
+      fetchJoke();
+    },[]);
+    
+    useEffect(()=>{
+      setFavJoke(getJoke());
+    },[]);
+
+    useEffect(()=>{
+      storeJoke(favJoke);
+    },[favJoke]);
+
     const storeJoke=(joke:string) =>{
         localStorage.setItem('joke', JSON.stringify(joke));
+        console.log('[POST] ' + joke);
     }
 
     const getJoke:any=()=>{
-      let res = JSON.parse(localStorage.getItem('joke') || 'Not found');
-
+      let res = JSON.parse(localStorage.getItem('joke') ?? '{}');
+      console.log('[GET] ' + res);
       return res;
     }
 
@@ -36,24 +51,34 @@ const DadJoke = () => {
     direction="column"
     alignItems="center"
     justifyContent="center"
-    style={{marginTop:'20%', minWidth:'100%' }}>
+    style={{marginTop:'15%', minWidth:'100%' }}>
       <Grid item xs={3}>
-        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={1}>
-          <Grid item>
-            <Grid container direction="row">
-              <Grid item>
-              <Button onClick={() => fetchJoke()} variant="contained">
-              Get a new joke
-              </Button>
-              
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
+        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={3}>
+        <Grid item>
             {joke && (
             <Typography variant="body1" style={{color:theme.palette.text.primary}}>{joke}</Typography>
             )}
           </Grid>
+          <Grid item>
+            <Grid container direction="row" spacing={1}>
+              <Grid item>
+                <Button onClick={() => fetchJoke()} variant="contained">
+                Get a new joke
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button onClick={()=> setFavJoke(joke)} variant="contained">
+                  Set as favorite
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+            {favJoke && (
+             <Grid item>
+                <Typography variant="h5" style={{color:theme.palette.text.primary}}>Favorite joke:</Typography>
+                <Typography variant="body1" style={{color:theme.palette.text.primary}}>{favJoke}</Typography>
+              </Grid>
+            )}
         </Grid>
       </Grid>
     </Grid>
